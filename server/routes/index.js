6f8831785c758726c52250772e5237fe8491b51a
ArtKhlg <shhessa@bk.ru>
2023-05-04 02:18:26 +0300

    int-mag theory on practice 03.05

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.use("/auth", require("./auth.routes"));
-router.use("/categories", require("./categories.routes"));
-router.use("/products", require("./products.routes"));
+router.use("/categories", require("./category.routes"));
+router.use("/products", require("./product.routes"));
+router.use("/user", require("./user.routes"));
 
 module.exports = router;
